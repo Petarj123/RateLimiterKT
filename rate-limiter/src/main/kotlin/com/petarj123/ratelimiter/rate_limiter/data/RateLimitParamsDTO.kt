fb9541842111d88ce2188a89415e89484f13842b
Petarj123 <76112835+Petarj123@users.noreply.github.com>
2023-10-07 19:32:27 +0200

    Create RateLimitParamsDTO.kt

---
+++ rate-limiter/src/main/kotlin/com/petarj123/ratelimiter/rate_limiter/data/RateLimitParamsDTO.kt
@@ -0,0 +1,10 @@
+package com.petarj123.ratelimiter.rate_limiter.data
+
+data class RateLimitParamsDTO(val identifier: String,
+                              val maxRequests: Int,
+                              val timeWindowSeconds: Long,
+                              val bucketCapacity: Int,
+                              val bucketRefillRate: Int,
+                              val bucketRefillTime: Long,
+                              val suspensionDuration: Long,
+                              val suspensionThreshold: Int)
