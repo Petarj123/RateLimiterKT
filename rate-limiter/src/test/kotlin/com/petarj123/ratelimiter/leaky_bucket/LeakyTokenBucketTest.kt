cfe15882b6892083e9e0b4e452bd5aea80a77fc5
Petarj123 <76112835+Petarj123@users.noreply.github.com>
2023-10-10 17:54:21 +0200

    TokenBucket and LeakyBucket tests

---
+++ rate-limiter/src/test/kotlin/com/petarj123/ratelimiter/leaky_bucket/LeakyTokenBucketTest.kt
@@ -0,0 +1,110 @@
+package com.petarj123.ratelimiter.leaky_bucket
+
+import com.petarj123.ratelimiter.rate_limiter.config.RateLimiterProperties
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.data.redis.core.StringRedisTemplate
+import org.springframework.test.context.TestPropertySource
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@TestPropertySource(properties = ["rate-limiter.algorithm=LEAKY_BUCKET", "rate-limiter.default-bucket-capacity=10", "rate-limiter.default-drip-rate=10"])
+class LeakyTokenBucketTest {
+    @Autowired
+    lateinit var mockMvc: MockMvc
+    @Autowired
+    lateinit var stringRedisTemplate: StringRedisTemplate
+    @Autowired
+    lateinit var rateLimiterProperties: RateLimiterProperties
+
+    @AfterEach
+    fun tearDown() {
+        stringRedisTemplate.delete("leaky:127.0.0.1")
+    }
+
+    @Test
+    fun `exceeds bucket capacity immediately, should return status 429`() {
+        repeat(10) {  // Exhaust the capacity
+            mockMvc.perform(get("/test/hi")).andExpect(status().isOk)
+        }
+        mockMvc.perform(get("/test/hi"))
+            .andExpect(status().isTooManyRequests)
+    }
+
+    @Test
+    fun `does not exceed bucket capacity immediately, should return status 200`() {
+        repeat(10) {
+            mockMvc.perform(get("/test/hi")).andExpect(status().isOk)
+        }
+    }
+
+    @Test
+    fun `bucket drips at expected rate`() {
+        repeat(5) {
+            mockMvc.perform(get("/test/hi")).andExpect(status().isOk)
+        }
+
+        // Wait for tokens to drip (restore) using the drip rate
+        Thread.sleep((rateLimiterProperties.defaultDripRate * 1000 * 5).toLong())
+
+        // After waiting for 5 seconds (defaultDripRate = 1 token/sec), we should have 5 more tokens
+        repeat(5) {
+            mockMvc.perform(get("/test/hi")).andExpect(status().isOk)
+        }
+    }
+
+    @Test
+    fun `ensure long waits restore the bucket`() {
+        repeat(10) {
+            mockMvc.perform(get("/test/hi")).andExpect(status().isOk)
+        }
+
+        // Wait for the entire bucket to fill up
+        Thread.sleep((rateLimiterProperties.defaultBucketCapacity * rateLimiterProperties.defaultDripRate * 1000).toLong())
+
+        // The bucket should be full again
+        repeat(10) {
+            mockMvc.perform(get("/test/hi")).andExpect(status().isOk)
+        }
+    }
+
+    @Test
+    fun `intermittent requests respect the drip rate`() {
+        repeat(5) {
+            mockMvc.perform(get("/test/hi")).andExpect(status().isOk)
+            Thread.sleep((rateLimiterProperties.defaultDripRate * 1000).toLong())
+        }
+    }
+
+    @Test
+    fun `ensure tokens do not drip if bucket is not used`() {
+        // The idea here is to verify that unused tokens don't lead to "overflow", even if we wait a long time.
+        Thread.sleep((60000).toLong()) // Double the required time
+
+        repeat(10) {
+            mockMvc.perform(get("/test/hi")).andExpect(status().isOk)
+        }
+    }
+
+    @Test
+    fun `verify rate limiter respects IP whitelist`() {
+        rateLimiterProperties.whitelistedIps = setOf("127.0.0.1")
+
+        repeat(20) {
+            mockMvc.perform(get("/test/hi")).andExpect(status().isOk)
+        }
+    }
+
+    @Test
+    fun `verify rate limiter respects IP blacklist`() {
+        rateLimiterProperties.blacklistedIps = setOf("127.0.0.1")
+
+        mockMvc.perform(get("/test/hi")).andExpect(status().isForbidden)
+    }
+}
--- rate-limiter/src/test/kotlin/com/petarj123/ratelimiter/token_bucket/TokenBucketTest.kt
+++ rate-limiter/src/test/kotlin/com/petarj123/ratelimiter/token_bucket/TokenBucketTest.kt
@@ -1,7 +1,10 @@
 package com.petarj123.ratelimiter.token_bucket
 
 import com.petarj123.ratelimiter.interceptor.RateLimiterInterceptor
+import com.petarj123.ratelimiter.rate_limiter.config.RateLimiterProperties
 import com.petarj123.ratelimiter.rate_limiter.service.ClientSuspensionService
+import io.lettuce.core.KillArgs.Builder.user
+import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
@@ -21,24 +24,73 @@ import org.springframework.test.web.servlet.setup.DefaultMockMvcBuilder
 import org.springframework.test.web.servlet.setup.MockMvcBuilders
 import org.springframework.web.context.WebApplicationContext
 import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping
+import java.util.concurrent.TimeUnit
 
 
 @SpringBootTest
 @AutoConfigureMockMvc
-@TestPropertySource(properties = ["rate-limiter.algorithm=LEAKY_BUCKET", "rate-limiter.default-bucket-capacity=10"])
+@TestPropertySource(properties = ["rate-limiter.algorithm=TOKEN_BUCKET", "rate-limiter.default-bucket-capacity=10"])
 class TokenBucketTest() {
     @Autowired
     lateinit var mockMvc: MockMvc
+    @Autowired
+    lateinit var stringRedisTemplate: StringRedisTemplate
+    @Autowired
+    lateinit var rateLimiterProperties: RateLimiterProperties
 
+    @AfterEach
+    fun tearDown() {
+        stringRedisTemplate.delete("bucket:127.0.0.1")
+    }
 
     @Test
-    fun `test token bucket`() {
-        repeat(100) {
+    fun `exceeds bucket capacity, should return status 429`() {
+        repeat(10) {
             mockMvc.perform(get("/test/hi"))
         }
         mockMvc.perform(get("/test/hi"))
             .andExpect(status().isTooManyRequests)
     }
+    @Test
+    fun `does not exceed bucket capacity, should return status 200`() {
+        repeat(10) {
+            mockMvc.perform(get("/test/hi")).andExpect(status().isOk)
+        }
+    }
 
+    @Test
+    fun `bucket refills at expected rate`() {
+        // Exhaust the bucket initially
+        repeat(10) {
+            mockMvc.perform(get("/test/hi")).andExpect(status().isOk)
+        }
+
+        // Ensure a request fails immediately after
+        mockMvc.perform(get("/test/hi")).andExpect(status().isTooManyRequests)
+
+        // Wait for a time equivalent to half the refill rate and check if you can make a request
+        Thread.sleep(rateLimiterProperties.defaultBucketRefillTime / 2 * 1000)
+        mockMvc.perform(get("/test/hi")).andExpect(status().isTooManyRequests)
+
+        // Wait for another half, completing the refill duration, then check if a request passes
+        Thread.sleep(rateLimiterProperties.defaultBucketRefillTime / 2 * 1000)
+        mockMvc.perform(get("/test/hi")).andExpect(status().isOk)
+    }
+
+
+    @Test
+    fun `bucket refills partially and only allows equivalent number of requests`() {
+        // Make initial requests to use half the tokens
+        repeat(5) {
+            mockMvc.perform(get("/test/hi")).andExpect(status().isOk)
+        }
+        // Wait for a full refill duration
+        Thread.sleep(rateLimiterProperties.defaultBucketRefillTime / 2 * 1000)
+        // Ensure only 5 more requests can be made before hitting the rate limit
+        repeat(5) {
+            mockMvc.perform(get("/test/hi")).andExpect(status().isOk)
+        }
+        mockMvc.perform(get("/test/hi")).andExpect(status().isTooManyRequests)
+    }
 
 }
